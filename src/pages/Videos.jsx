d6baa3bd2f56df2f1ede28179d3c2b827bf5a5a0
bestyjin <bestyjin0604@gmail.com>
2022-11-14 15:52:21 +0900

    get videos' title using Mock data

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "../components/VideoCard";
 
 function Videos(props) {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? `🔎${keyword}` : `🔥`} </div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], async () => {
+    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => res.json())
+      .then((data) => data.items);
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `🔎${keyword}` : `🔥`} </div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Somthing is wrong</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
 
 export default Videos;
