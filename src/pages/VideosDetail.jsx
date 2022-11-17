a8c9cbc65cdf8005938451c78ecb825c774305fb
bestyjin <bestyjin0604@gmail.com>
2022-11-17 16:35:43 +0900

    Video Detail - ChannelInfo

--- src/pages/VideosDetail.jsx
+++ src/pages/VideosDetail.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import ChannelInfo from "../components/ChannelInfo";
+import RelatedVideos from "../components/RelatedVideos";
 
-function VideosDetail(props) {
-    return (
-        <div>VideosDetail
-            
+function VideosDetail() {
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
+  return (
+    <section>
+      <article>
+        <iframe
+          id="player"
+          type="text/html"
+          width="100%"
+          height="640"
+          src={`http://www.youtube.com/embed/${video.id}`}
+          frameBorder="0"
+        />
+        <div>
+          <h2>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle} />
         </div>
-    );
+        <pre>{description}</pre>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
+  );
 }
 
 export default VideosDetail;
